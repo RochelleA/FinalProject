12d48028889cd8d3b230a4a0208d0691d42c7281
RochelleA <rochelle.allen@kcl.ac.uk>
2015-06-21 16:22:12 +0100

    I received the changes without causing an erro. Can I do the same in the other direcction

--- FinalProjectSoftware/src/graph1/Test.java
+++ FinalProjectSoftware/src/graph1/Test.java
@@ -3,5 +3,6 @@ package graph1;
 public class Test {
 	public static void main(String[] args){
 		System.out.println("This might work");
+		System.out.println("It worked. Does it work the other way");
 	}
 }
