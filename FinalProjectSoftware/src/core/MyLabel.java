d2d15aefb76ba1c86bb89d05735744e7688e6e25
RochelleA <r_j_allen@yahoo.co.uk>
2015-07-05 13:00:38 +0100

    Add test case for MyLabellings restricted the add to label if the vertex is already labelled something different.

---
+++ FinalProjectSoftware/src/core/MyLabel.java
@@ -0,0 +1,13 @@
+package core;
+
+public class MyLabel {
+	final static String IN="IN";
+	final static String OUT="OUT";
+	final static String UNDEC="UNDEC";
+	final static String NONE= "NONE";
+
+	public MyLabel() {
+			
+	}
+
+}
---
+++ FinalProjectSoftware/src/gui/MyPluggableGraphMouse.java
@@ -0,0 +1,14 @@
+package gui;
+
+import edu.uci.ics.jung.visualization.control.PluggableGraphMouse;
+
+public class MyPluggableGraphMouse {
+	
+	PluggableGraphMouse gm;
+
+	public MyPluggableGraphMouse() {
+		 gm = new PluggableGraphMouse(); 
+	}
+
+}
+
